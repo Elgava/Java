8e4478dbf9ffe8e357989300986899a92f5edbd1
elgin <elginm08@gmail.com>
2021-09-27 15:02:23 +0200

    first java commit

---
+++ JavaBeginnerClasswork/src/main/java/Lesson_2/HellowWorld.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Lesson_2;
+
+/**
+ *
+ * @author elgin
+ */
+public class HellowWorld {
+    public static String firstName = "John";
+    public static String lastName = "Wick";
+        
+    public static void main(String[] args) {
+        System.out.println(firstName + " " + lastName);
+        System.out.println("My first Java class.");
+    }
+}
