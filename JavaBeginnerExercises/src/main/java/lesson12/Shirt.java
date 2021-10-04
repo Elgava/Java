ba1ad1f1136a584899ce352d1ff014e966689237
elgin <elginm08@gmail.com>
2021-10-04 11:12:46 +0200

    java

--- JavaBeginnerExercises/src/main/java/lesson12/Shirt.java
+++ JavaBeginnerExercises/src/main/java/lesson12/Shirt.java
@@ -9,36 +9,36 @@ package lesson12;
  *
  * @author elgin
  */
-//public class Shirt extends CLothing{
-//        private char fit = 'U';
-//        private String sleeve;
-//        
-//        public Shirt(int itemID, String description, char colorCode, double price, char fit, String sleeve){
-//            super(itemID, description, colorCode, price);
-//            this.fit = fit;
-//            this.sleeve = sleeve;
-//    }
-//        //override
-//        public void display(){
-//          super.display();
-//            System.out.println("fit: " + getFit());
-//            System.out.println("Sleees: " + getSleeve());
-//        }
-//
-//    public char getFit() {
-//        return fit;
-//    }
-//
-//    public void setFit(char fit) {
-//        this.fit = fit;
-//    }
-//
-//    public String getSleeve() {
-//        return sleeve;
-//    }
-//
-//    public void setSleeve(String sleeve) {
-//        this.sleeve = sleeve;
-//    }
-//       
-//}
+public class Shirt extends CLothing{
+        private char fit = 'U';
+        private String sleeve;
+        
+        public Shirt(int itemID, String description, char colorCode, double price, char fit, String sleeve){
+            super(itemID, description, colorCode, price);
+            this.fit = fit;
+            this.sleeve = sleeve;
+    }
+        //override
+        public void display(){
+          super.display();
+            System.out.println("fit: " + getFit());
+            System.out.println("Sleees: " + getSleeve());
+        }
+
+    public char getFit() {
+        return fit;
+    }
+
+    public void setFit(char fit) {
+        this.fit = fit;
+    }
+
+    public String getSleeve() {
+        return sleeve;
+    }
+
+    public void setSleeve(String sleeve) {
+        this.sleeve = sleeve;
+    }
+       
+}
--- JavaBeginnerExercises/src/main/java/lesson12/Trouser.java
+++ JavaBeginnerExercises/src/main/java/lesson12/Trouser.java
@@ -9,39 +9,39 @@ package lesson12;
  *
  * @author elgin
  */
-//public class Trouser {
-//    public class trouser extends CLothing{
-//        private char waist_size = 'U';
-//        private String leg_length;
-//        private char gender;
-//        
-//        public trouser(int itemID, String description, char colorCode, double price, char fit, String sleeve){
-//            super(itemID, description, colorCode, price);
-//            this.waist_size = waist_size;
-//            this.leg_length = leg_length;
-//            this.gender = gender;
-//    }
-//        
-//        public void display(){
-//          super.display();
-//            System.out.println("waist size: " + getWaist_size());
-//            System.out.println("Sleees: " + getLeg_length());
-//        }
-//
-//        public char getWaist_size() {
-//            return waist_size;
-//        }
-//
-//        public void setWaist_size(char waist_size) {
-//            this.waist_size = waist_size;
-//        }
-//
-//        public String getLeg_length() {
-//            return leg_length;
-//        }
-//
-//        public void setLeg_length(String leg_length) {
-//            this.leg_length = leg_length;
-//        }
-//    }
-//}
+    //public class Trouser {
+    public class Trouser extends CLothing{
+        private char waist_size;
+        private String leg_length;
+        private char gender;
+        
+        public Trouser(int itemID, String desc, char colorCode, double price, char fit, String sleeve){
+            super(itemID, desc, colorCode, price);
+            this.waist_size = waist_size;
+            this.leg_length = leg_length;
+            this.gender = gender;
+    }
+        
+        public void display(){
+          super.display();
+            System.out.println("waist size: " + getWaist_size());
+            System.out.println("Sleees: " + getLeg_length());
+        }
+
+        public char getWaist_size() {
+            return waist_size;
+        }
+
+        public void setWaist_size(char waist_size) {
+            this.waist_size = waist_size;
+        }
+
+        public String getLeg_length() {
+            return leg_length;
+        }
+
+        public void setLeg_length(String leg_length) {
+            this.leg_length = leg_length;
+        }
+    }
+}
