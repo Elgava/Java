30a1f4e69289fcaaa8e52216b3b1efa200368280
elgin <elginm08@gmail.com>
2021-10-19 10:40:31 +0200

    F1_V11

---
+++ Java_vid_projects/float_and_double/src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.awt.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        float Min_float_value = Float.MIN_VALUE;
+        float Max_float_value = Float.MAX_VALUE;
+        System.out.println("float minimum value = " + Min_float_value);
+        System.out.println("Float maximum value = " + Max_float_value);
+
+        double Min_double_value = Double.MIN_VALUE;
+        double Max_double_value = Double.MAX_VALUE;
+        System.out.println("double minimum value = " + Min_double_value);
+        System.out.println("double maximum value = " + Max_double_value);
+    }
+}
