29dda933aa858ff41b5b30832209d00c8b3d2f82
elgin <elginm08@gmail.com>
2021-10-27 09:13:55 +0200

    27-10-2021(1)

---
+++ classwork_javaA/AbstractElectronicDevices/src/main/java/example/ElectronicDevice.java
@@ -0,0 +1,26 @@
+package example;
+
+public abstract class ElectronicDevice {
+
+    public abstract void turnOn();
+
+    public abstract void turnOff();
+
+    public void setState(PowerState state) {
+        switch (state) {
+            case OFF:
+                System.out.println(state.getDescription());
+                break;
+            case ON:
+                System.out.println(state.getDescription());
+                break;
+            case SUSPEND:
+                System.out.println(state.getDescription());
+                break;
+            default:
+                System.out.println("There is no suh state");
+                break;
+        }
+    }
+
+}
