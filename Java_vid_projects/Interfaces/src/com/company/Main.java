0d829fdca6bc95820ee094b69df0a266e2add563
elgin <elginm08@gmail.com>
2021-11-24 11:21:37 +0200

    CH9V2

---
+++ Java_vid_projects/Interfaces/src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        ITelephone timsPhone;
+        timsPhone = new DeskPhone(123469);
+        timsPhone.powerOn();
+        timsPhone.callPhone(123469);
+        timsPhone.answer();
+
+        timsPhone = new MobilePhone(691234);
+        timsPhone.powerOn();
+        timsPhone.callPhone(691234);
+        timsPhone.answer();
+    }
+}
